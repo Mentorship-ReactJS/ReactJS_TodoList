cafce8ff08f0576c5e860781b4e5620c905bf26c
HuyND11 <huy.nguyen23@student.passerellesnumeriques.org>
2022-12-05 01:04:47 +0700

    Creat new todo task, store in state

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,10 +1,29 @@
 import { TextField } from "@mui/material";
+import { useState } from "react";
 
-const AddTask = () => {
+const AddTask = ({ onAddTodoList }) => {
+  const [title, setTitle] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onAddTodoList(title);
+    setTitle("");
+  };
+
+  console.log(title);
 
   return (
-    <form>
-      <TextField label="Add a new task" variant="outlined" size="small" fullWidth = {true} />
+    <form onSubmit={handleSubmit}>
+      <TextField
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+        value={title}
+        label="Add a new task"
+        variant="outlined"
+        size="small"
+        fullWidth={true}
+      />
     </form>
   );
 };
--- src/views/TodoList.jsx
+++ src/views/TodoList.jsx
@@ -1,12 +1,28 @@
 import { Container } from "@mui/material";
 import AddTask from "../components/AddTask";
 import TaskList from "../components/TaskList";
+import { useState } from "react";
 
 const TodoList = () => {
+  const [todoList, setTodoList] = useState([]);
+
+  const handleAddTodoList = (title) => {
+    setTodoList([
+      ...todoList,
+      {
+        id: todoList.length + 1,
+        title: title,
+        done: false,
+      },
+    ]);
+  };
+
+  console.log(todoList);
+
   return (
     <Container maxWidth="sm">
-      <AddTask />
-      <TaskList />
+      <AddTask onAddTodoList={handleAddTodoList} />
+      <TaskList list={todoList} />
     </Container>
   );
 };
