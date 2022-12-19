13b58745d1bf43b3c086cffb8ac9dc363e5e1525
HuyND11 <huy.nguyen23@student.passerellesnumeriques.org>
2022-12-19 21:43:45 +0700

    Update code following mentor comment

--- src/views/TodoList.jsx
+++ src/views/TodoList.jsx
@@ -3,6 +3,7 @@ import AddTask from "../components/AddTask";
 import TaskList from "../components/TaskList";
 import { useState, useCallback } from "react";
 import Statusbar from "../components/StatusBar";
+import { useMemo } from "react";
 
 function TodoList() {
   const [todoList, setTodoList] = useState([]);
@@ -22,14 +23,16 @@ function TodoList() {
     [todoList]
   );
 
-  const fitterTodoList = todoList.filter((task) => {
-    if (fitterStatus === "all") {
-      return true;
-    } else if (fitterStatus === "completed") {
-      return task.done === true;
-    }
-    return task.done === false;
-  });
+  const filteredTodos = useMemo(() => {
+    todoList.filter((task) => {
+      if (fitterStatus === "all") {
+        return true;
+      } else if (fitterStatus === "completed") {
+        return task.done === true;
+      }
+      return task.done === false;
+    });
+  }, [todoList, fitterStatus]);
 
   const handleChangFitterStatus = useCallback((status) => {
     setFitterStatus(status);
@@ -39,7 +42,7 @@ function TodoList() {
     <Container maxWidth="sm">
       <AddTask onAddTodoList={handleAddTodoList} />
       <Statusbar onChangeStatus={handleChangFitterStatus} />
-      <TaskList tasks={fitterTodoList} />
+      <TaskList tasks={filteredTodos} />
     </Container>
   );
 }
